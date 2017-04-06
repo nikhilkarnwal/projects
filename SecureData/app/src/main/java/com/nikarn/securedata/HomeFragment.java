491a5e62725cc12fdaa8e13ce66b640142a0ba6d
Nikhil Karnwal <nikarn@microsoft.com>
2017-04-06 20:58:40 +0530

    updated homefragment

--- SecureData/app/src/main/java/com/nikarn/securedata/HomeFragment.java
+++ SecureData/app/src/main/java/com/nikarn/securedata/HomeFragment.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.nikarn.securedata.Utils.Utility;
 
 
 public class HomeFragment extends Fragment {
@@ -24,7 +27,22 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        Button loginButton = (Button) rootView.findViewById(R.id.bttn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String passText = ((EditText) rootView.findViewById(R.id.edit_text_password)).getText().toString();
+                validatePassword(passText);
+            }
+        });
+    }
+
+    private void validatePassword(String passText) {
+        if (Utility.isValidPassword(passText)){
+
+        }
     }
 }
